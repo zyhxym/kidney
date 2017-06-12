b683127cc1cbc3e93a77225a9788d6e4dc3fe7a6
Jiazhen Xiong <xiongjzh@gmail.com>
2017-06-12 20:08:54 +0800

    病历页增加聊天记录

--- www/js/app.js
+++ www/js/app.js
@@ -669,14 +669,14 @@ angular.module('kidney',[
             }
         })
     .state('tab.group-detail', {
-            url: '/groups/detail',
+            url: '/groups/:teamId/detail',
             views: {
                 'tab-groups': {
                     templateUrl: 'partials/group/group-detail.html',
                     controller: 'GroupDetailCtrl'
                 }
-            },
-            params:{teamId:null}
+            }
+            // params:{teamId:null}
         })
     .state('tab.group-qrcode', {
             url: '/groups/qrcode',
@@ -695,6 +695,16 @@ angular.module('kidney',[
                 'tab-groups': {
                     templateUrl: 'partials/group/group-chat.html',
                     controller: 'GroupChatCtrl'
+                }
+            }
+        })
+    .state('tab.view-chat', {
+        //'0':团队交流  '1': 未结束病历  '2':已结束病历
+            url: '/viewchat/:doctorId/:patientId',
+            views: {
+                'tab-groups': {
+                    templateUrl: 'partials/group/view-chat.html',
+                    controller: 'viewChatCtrl'
                 }
             }
         })
