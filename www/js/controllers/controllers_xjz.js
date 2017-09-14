8088bf21ef1b21cd9d403b455aae86fcf606b15e
zyhxym <411593859@qq.com>
2017-09-14 15:38:43 +0800

    news表userRole修改

--- www/js/controllers/controllers_xjz.js
+++ www/js/controllers/controllers_xjz.js
@@ -656,7 +656,7 @@ angular.module('xjz.controllers', ['ionic', 'kidney.services'])
         if ($scope.msgs.length == 0) return
         var lastMsg = $scope.msgs[$scope.msgs.length - 1]
         if (lastMsg.fromID == $scope.params.UID) return
-        return New.insertNews({ userId: lastMsg.targetID, sendBy: lastMsg.fromID, type: $scope.params.newsType, userRole: $scope.params.type == '2' ? 'doctor' : 'patient', readOrNot: 1 })
+        return New.insertNews({ userId: lastMsg.targetID, sendBy: lastMsg.fromID, type: $scope.params.newsType, userRole: 'doctor', readOrNot: 1 })
       }
     })
   })
@@ -739,7 +739,7 @@ angular.module('xjz.controllers', ['ionic', 'kidney.services'])
         })
         $scope.counselstatus = 1
       }
-      New.insertNews({ userId: $scope.params.UID, sendBy: $scope.params.chatId, type: $scope.params.newsType, userRole: $scope.params.type == '2' ? 'doctor' : 'patient', readOrNot: 1 })
+      New.insertNews({ userId: $scope.params.UID, sendBy: $scope.params.chatId, type: $scope.params.newsType, userRole: 'doctor', readOrNot: 1 })
     }
   })
   /**
@@ -1876,7 +1876,7 @@ angular.module('xjz.controllers', ['ionic', 'kidney.services'])
         if ($scope.msgs.length == 0) return
         var lastMsg = $scope.msgs[$scope.msgs.length - 1]
         if (lastMsg.fromID == $scope.params.UID) return
-        return New.insertNews({ userId: $scope.params.UID, sendBy: lastMsg.targetID, type: $scope.params.newsType, readOrNot: 1, caseType: $scope.params.teamId})
+        return New.insertNews({ userId: $scope.params.UID, sendBy: lastMsg.targetID, type: $scope.params.newsType, readOrNot: 1, userRole: 'doctor', caseType: $scope.params.teamId})
       }
     })
     imgModalInit()
@@ -1911,7 +1911,7 @@ angular.module('xjz.controllers', ['ionic', 'kidney.services'])
       $scope.$apply(function () {
         insertMsg(data.msg)
       })
-      New.insertNews({userId: $scope.params.UID, sendBy: $scope.params.groupId, type: $scope.params.newsType, readOrNot: 1, caseType: $scope.params.teamId})
+      New.insertNews({userId: $scope.params.UID, sendBy: $scope.params.groupId, type: $scope.params.newsType, readOrNot: 1, userRole: 'doctor', caseType: $scope.params.teamId})
     }
   })
   $scope.$on('im:messageRes', function (event, data) {
