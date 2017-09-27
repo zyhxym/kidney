89b5c9c42294ed6d5d63d446c99ee3265ac97301
zyyatgit <yyzhangyingying@hotmail.com>
2017-09-27 19:52:05 +0800

    首页消息完善

--- www/js/controllers/controllers_fyl.js
+++ www/js/controllers/controllers_fyl.js
@@ -144,20 +144,24 @@ angular.module('fyl.controllers', ['ionic', 'kidney.services'])
       console.log(data)
       if (data.results.length) {
         if (data.results[0].type == 11) {
-          str1 = data.results[0].url.split('"fromName":')[1]
-          // str2 = str1.split(':')[1]
-          str3 = str1.split(',"fromID"')[0]
-          document.getElementById('newMes').innerText = '最新消息：患者' + str3 + '给您发来一条消息“' + data.results[0].description + '”'
+          // str1 = data.results[0].url.split('"fromName":')[1]
+          // str3 = str1.split(',"fromID"')[0]
+          str3 = angular.fromJson(data.results[0].url).fromName
+          $scope.newMes = '最新消息：患者' + str3 + '给您发来一条消息“' + data.results[0].description + '”'
         } else if (data.results[0].type == 15) {
-          document.getElementById('newMes').innerText = '您有一条病例讨论新消息：“' + data.results[0].description + '”'
+          $scope.newMes = '您有一条病例讨论新消息：“' + data.results[0].description + '”'
         } else if (data.results[0].type == 13) {
-          document.getElementById('newMes').innerText = '您有一条团队新消息：“' + data.results[0].description + '”'
+          $scope.newMes = '您有一条团队新消息：“' + data.results[0].description + '”'
         } else if (data.results[0].type == 12) {
-          str1 = data.results[0].url.split('"fromName":')[1]
-          str2 = str1.split(',"fromUser"')[0]
-          document.getElementById('newMes').innerText = '最新消息：医生' + str2 + '给您发来一条消息“' + data.results[0].description + '”'
+          // str1 = data.results[0].url.split('"fromName":')[1]
+          // str2 = str1.split(',"fromUser"')[0]
+          str2 = angular.fromJson(data.results[0].url).fromName
+          $scope.newMes = '最新消息：医生' + str2 + '给您发来一条消息“' + data.results[0].description + '”'
         } else {
-          document.getElementById('newMes').innerText = '最新消息：' + data.results[0].description
+          $scope.newMes = '最新消息：' + data.results[0].description
+        }
+        if ($scope.newMes.length >= 80) {
+          $scope.newMes = $scope.newMes.slice(0, 79) + '...'
         }
 
         for (i = 0; i < data.results.length; i++) {
