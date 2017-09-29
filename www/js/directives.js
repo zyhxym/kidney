237351d53f780392ec021a2fb546fbe3251053af
zyyatgit <yyzhangyingying@hotmail.com>
2017-09-29 14:26:26 +0800

    医生搜索bug修复

--- www/js/directives.js
+++ www/js/directives.js
@@ -4,16 +4,16 @@ angular.module('kidney.directives', ['kidney.services'])
   return {
     restrict: 'AE',
     scope: {
-      input: '=',  // 这里可以直接用input获取父scope(包括兄弟元素)中ng-model的值, 传递给本directive创建的isolate scope使用, template也属于当前isolate scope
-      origin: '=',
-      curr: '='
+      input: '='  // 这里可以直接用input获取父scope(包括兄弟元素)中ng-model的值, 传递给本directive创建的isolate scope使用, template也属于当前isolate scope
+      // origin: '=',
+      // curr: '='
     },
         // replace: true,   //使用replace之后, 本元素的click不能删除输入框中的内容, 原因大致可以理解为: 父元素被替换后, scope.$apply没有执行对象
     template: "<span ng-if='input' class='icon ion-android-close placeholder-icon' on-tap='clearInput()'></span>",
     controller: function ($scope, $element, $attrs) {
       $scope.clearInput = function () {
         $scope.input = ''
-        $scope.curr = $scope.origin
+        // $scope.curr = $scope.origin
       }
     }
   }
